ad7d75a4e2b29d3fbf42de70c567c723f6207577
Nguyen-Khanh01 <93971164+Nguyen-Khanh01@users.noreply.github.com>
2022-03-21 22:26:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh `,
+    btnIntro: 'hihi',
+    title: 'cậu làm người yêu mình nha ♥️',
+    desc: 'thoát ra là đồng ý nha 😆',
+    btnYes: 'Đồng Ý😍',
+    btnNo: 'No',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'yêu từ cái nhìn đầu tiên',
+    mess: 'Anh biết mà . call vidoe ngay thôi chứ còn chờ gì nữa',
+    // messDesc: 'Tối nay  anh qua đón nhé công chúa.',
+    btnAccept: 'Ok nè',
+    messLink: 'https://www.facebook.com/messages/t/3753253041446443/' 
+}
